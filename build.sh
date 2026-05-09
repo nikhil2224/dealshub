9b4cd40f5ceba6e9b115ec55f18a4e87175546f1
nikhil cs <nikhilcs526@gmail.com>
2026-05-09 20:08:37 +0530

    Include pre-seeded database and fix build script for Vercel

--- build.sh
+++ build.sh
@@ -3,11 +3,5 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
-
-# Seed data
-python seed_data.py
-
 # Collect static files
 python manage.py collectstatic --noinput
